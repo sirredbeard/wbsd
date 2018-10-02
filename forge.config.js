813de3424b3b40c6d571bc7204f7323e7657cc46
Hayden <33820650+sirredbeard@users.noreply.github.com>
2018-10-02 19:44:35 -0400

    Update forge.config.js

--- forge.config.js
+++ forge.config.js
@@ -7,14 +7,11 @@ module.exports = {
       unpack: '**/images/*.img'
     },
     icon: path.resolve(__dirname, 'assets', 'icon'),
-    appBundleId: 'com.felixrieseberg.windows95',
+    appBundleId: 'com.whitewaterfoundry.wbsd',
     appCategoryType: 'public.app-category.developer-tools',
     win32metadata: {
-      CompanyName: 'Felix Rieseberg',
-      OriginalFilename: 'windows95',
-    },
-    osxSign: {
-      identity: 'Developer ID Application: Felix Rieseberg (LT94ZKYDCJ)'
+      CompanyName: 'Whitewater Foundry',
+      OriginalFilename: 'wbsd',
     },
   },
   makers: [
@@ -22,32 +19,16 @@ module.exports = {
       name: '@electron-forge/maker-squirrel',
       platforms: ['win32'],
       config: {
-        name: 'windows95',
-        authors: 'Felix Rieseberg',
-        exe: 'windows95.exe',
+        name: 'wbsd',
+        authors: 'Whitewater Foundry',
+        exe: 'wbsd.exe',
         noMsi: true,
         remoteReleases: '',
-        setupExe: `windows95-${package.version}-setup-${process.arch}.exe`,
+        setupExe: `wbsd-${package.version}-setup-${process.arch}.exe`,
         setupIcon: path.resolve(__dirname, 'assets', 'icon.ico'),
         certificateFile: process.env.WINDOWS_CERTIFICATE_FILE,
         certificatePassword: process.env.WINDOWS_CERTIFICATE_PASSWORD
       }
     },
-    {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin']
-    },
-    {
-      name: '@electron-forge/maker-deb',
-      platforms: ['linux']
-    },
-    {
-      name: '@electron-forge/maker-rpm',
-      platforms: ['linux']
-    },
-    {
-      name: '@electron-forge/maker-flatpak',
-      platforms: ['linux']
-    }
   ]
 };
